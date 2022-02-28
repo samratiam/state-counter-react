21849dc584c55d8061bdec533a13db057c32370f
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-28 12:31:54 +0545

    Create counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css"
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Count Number: {count} </p>
+
+      <button onClick={() => count >= 10 ? "" : setCount(count + 1)} >Increment Counter</button>
+      <button onClick={() => count <= 0 ? "" : setCount(count - 1)}>Decrement Counter</button>
+      <button onClick={() => setCount(0)}>Reset Counter</button>
+
     </div>
-  );
+  )
 }
 
 export default App;
